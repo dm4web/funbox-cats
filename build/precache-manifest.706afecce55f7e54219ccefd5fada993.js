29a3eb3212aac49a5d6e1f41c8f2bcb45cb42885
DanilaMolchanov <sadme28@gmail.com>
2020-08-23 18:51:44 +0300

    Add BUILD folder

---
+++ build/precache-manifest.706afecce55f7e54219ccefd5fada993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78eafbde4cf6c3a11d202d359af9b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9566ddcd0b676244def",
+    "url": "/static/css/main.c7f05695.chunk.css"
+  },
+  {
+    "revision": "e3034554f87948d44fa1",
+    "url": "/static/js/2.e371f7cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e371f7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9566ddcd0b676244def",
+    "url": "/static/js/main.1936fd8f.chunk.js"
+  },
+  {
+    "revision": "da821e3e17146a67b854",
+    "url": "/static/js/runtime-main.2c0deb60.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/static/media/Exo2.0-Thin.44bde874.otf"
+  }
+]);
